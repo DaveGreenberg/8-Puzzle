958d670dab34a0c97666ac9d25a062d752c6807c
Dave Greenberg <dxg347@case.edu>
2018-09-14 15:50:52 -0400

    Implemented getMisplacedTiles and getDistance to be used for h1 and h2 respectively for A* search.

--- src/State.java
+++ src/State.java
@@ -5,6 +5,7 @@
  */
 import java.lang.*;
 import java.util.Scanner;
+import java.util.PriorityQueue;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.*;
@@ -20,8 +21,25 @@ public class State
     //String to store the file name
     private String inputFile;
     
+    //Matrix to store the goal board
+    final Tile[][] goalState;
+    
     //String to store Scanner line
     //private static String str;
+    
+    public State()
+    {
+        this.goalState = new Tile[3][3];
+        goalState[0][0] = new Tile(0);
+        goalState[0][1] = new Tile(1);
+        goalState[0][2] = new Tile(2);
+        goalState[1][0] = new Tile(3);
+        goalState[1][1] = new Tile(4);
+        goalState[1][2] = new Tile(5);
+        goalState[2][0] = new Tile(6);
+        goalState[2][1] = new Tile(7);
+        goalState[2][2] = new Tile(8);
+    }
     
     /**
      * Constructor to accept a file
@@ -31,6 +49,14 @@ public class State
     {
         this.inputFile = file;
     } */
+    
+    /**
+     * Method to return the puzzle state/board
+     */
+    public Tile[][] getPuzzle()
+    {
+        return this.puzzle;
+    }
     
     /**
      * Set the puzzle state. The goal state  is "b12 345 678"
@@ -277,6 +303,105 @@ public class State
         }
         else if (command[0].equals("quit"))
             return;
+    }
+    
+    /**
+     * Method to check if the current state is the goal state
+     * @param s
+     * @return 
+     */
+    public boolean isGoalState(State s)
+    {
+        return s.printState().equals("b12 345 678");
+    }
+    
+    /**
+     * Method to return the number of misplaced tiles
+     * @param s
+     * @return 
+     */
+    public int getMisplacedTiles(State s)
+    {
+       Tile[][] board = s.getPuzzle();
+       int count = 0;
+       for (int i = 0; i < board.length; i++)
+       {
+         for (int j = 0; j < board[i].length; j++)
+         {
+             if (! (board[i][j].getValue() == this.goalState[i][j].getValue()))
+             {
+                 count++;
+             }
+         }
+       }
+       return count;
+    }
+    
+    /**
+     * Method to compute and return the total Manhattan distance for a given state/board
+     * @param s
+     * @return 
+     */
+    public int getDistance(State s) 
+    {
+       int totalDistance = 0;
+       Tile[][] board = s.getPuzzle();
+       for (int i = 0; i < board.length; i++)
+       {
+          for (int j = 0; j < board[i].length; j++) 
+          { 
+              int value = board[i][j].getValue();
+              if (value != 0)  //0 represents the blank tile, don't process it
+              {
+                  int goalRow = (value - 1) / board.length; // Row of tile in Goal State
+                  int goalCol = (value - 1) % board.length; // Column of tile in Goal State
+                  int distRow = i - goalRow;                // Distance to the goal row
+                  int distCol = j - goalCol;                // Distance to the goal Column
+                  totalDistance += Math.abs(distRow) + Math.abs(distCol);   //Distance could be negative, confirm it's positive 
+              } 
+          }
+       }
+       return totalDistance;
+    }
+    
+    /**
+     * Method to solve the puzzle using h1
+     * @param s
+     * @return 
+     */
+    public int h1(State s)
+    {
+        return 0;
+    }
+    
+    /**
+     * Method to calculate the total manhattan distance of each tile
+     * @param s
+     * @return 
+     */
+    public int h2(State s)
+    {
+       return 0; 
+    }
+    
+    /**
+     * Method to solve the 8-puzzle using A* search
+     * @param heuristic h1 or h2
+     * @return 
+     */
+    public String solveAStar(String heuristic)
+    {
+       PriorityQueue <State> stateTree = new PriorityQueue();  //Priority Queue to handle A* search, Need to make a Node class
+       if (heuristic.equals("h1"))  //# of misplaced tiles
+       {
+          return "h1"; 
+       }
+       else if (heuristic.equals("h2"))
+       {
+          return "h2";
+       }
+       else
+           return "Invalid Heuristic!";
     }
     
     /**
